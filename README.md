37d45b3acab186cf519b34bb0e3f49d0b49e7e21
MiguelBrazao <miguelbrazao0@gmail.com>
2026-04-10 10:15:14 +0100

    README update and visualization possible
    README update and modifications on the code so that mario can be visible while running random search (and others)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # NIAI_Practical_Project
 Nature-Inspired Artificial Intelligence Practical Project. The primary objective of this project is to design and implement an Evolutionary Algorithm (EA) capable of evolving autonomous controllers for Super Mario.
+
+# Como correr o random search
+dar docker compose up num terminal na pasta code
+depois ir ao local host e ver em que port está
+
+abrir anaconda prompt e meter 
+conda activate NIAI
+depois
+cd /d "<path_to_code>"
+
+depois meter 
+python mario_random_search_gp.py 42
+ou
+python mario_random_search_mlp.py 42
+sendo 42 uma seed (aleatório)
--- code/evaluation.py
+++ code/evaluation.py
@@ -69,7 +69,7 @@ def init_worker(agent_class):
 
     worker_agent = agent_class()
     if worker_task is None:
-        worker_task = TASK_TO_SOLVE(visualization=False, port=port, init_mario_mode=0)
+        worker_task = TASK_TO_SOLVE(visualization=True, port=port, init_mario_mode=0)
 
 
 def evaluate_individual(ind_info):
@@ -101,7 +101,7 @@ def evaluate(agent_class, ind_info):
     if worker_agent is None:
         worker_agent = agent_class()
     if worker_task is None:
-        worker_task = TASK_TO_SOLVE(visualization=False, port=port_list[0])
+        worker_task = TASK_TO_SOLVE(visualization=True, port=port_list[0])
     return evaluate_individual(ind_info)
 
 
