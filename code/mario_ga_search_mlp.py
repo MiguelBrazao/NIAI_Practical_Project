ba6755789effd35babe8f073f65acebfad6138ec
jnmonsanto <jnmonsanto@gmail.com>
2026-04-30 23:05:17 +0100

    sigma_decay tuned

--- code/mario_ga_search_mlp.py
+++ code/mario_ga_search_mlp.py
@@ -124,7 +124,7 @@ def update_sigma_stagnation(
 def genetic_algorithm(
         generations=500, population_size=100, tournament_k=4, elite_count=3, 
         crossover_rate=0.95, crossover_mask_prob=0.5, mutation_rate=0.1, 
-        sigma=0.5, sigma_decay=0.99, sigma_min=0.1, sigma_max=1.5,
+        sigma=0.5, sigma_decay=0.8, sigma_min=0.1, sigma_max=1.5,
         stagnation_ratio=0.05, population_restart_ratio=0.75
     ):
     """
